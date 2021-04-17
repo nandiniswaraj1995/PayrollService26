2bfa45d23e1f373e2dd12f4b40b644211ad734cf
unknown <nandiniswaraj13@gmail.com>
2021-04-17 22:13:22 +0530

    [Nandini] add values

---
+++ UC10.sql
@@ -0,0 +1,5 @@
+select * from employee_payroll
+insert into employee_payroll values
+('Terissa',45000,'2021-01-03','f','9876543210','xyz','marketiing',12000,500.23,1200,34000),
+('Terissa',45000,'2021-01-03','f','9876543210','xyz','sales',12000,500.23,1200,34000)
+
